2141d5cf4116bcc56eb421159df8c8ddec694b9d
Omam Muqadar <omammuqadar@gmail.com>
2023-03-20 13:50:59 +0500

    Initital REST API for TMS

--- controller/userController.js
+++ controller/userController.js
@@ -1,35 +1,65 @@
 const asyncHandler = require('express-async-handler')
 
+const User = require('../models/userModel')
+
 // @desc    Get user
 // @route   GET /backend/user
 // @access  Private
 const getUser = asyncHandler(async (req, res) => {
-  res.status(200).json({message:"Get User"})
+  const user = await User.find() 
+
+  res.status(200).json(user)
 })
 
 // @desc    Set user
 // @route   POST /backend/user
 // @access  Private
 const setUser = asyncHandler(async (req, res) => {
-  if(!req.body.user) {
+  if(!req.body.name) {
     res.status(400)
     throw new Error("Please add User Name")
   }
-  res.status(200).json({message:"Set User"})
+
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email
+  })
+
+  res.status(200).json(user)
 })
 
 // @desc    Update user
 // @route   PUT /backend/user/:id
 // @access  Private
 const updateUser = asyncHandler(async (req, res) => {
-  res.status(200).json({message:`Update User ${req.params.id}`})
+  const user = await User.findById(req.params.id)
+
+  if(!user) {
+    res.status(400)
+    throw new Error("User Not Found")
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body,{
+    new: true,
+  })
+
+  res.status(200).json(updatedUser)
 })
 
 // @desc    Delete user
 // @route   DELETE /backend/user/:id
 // @access  Private
 const deleteUser = asyncHandler(async (req, res) => {
-  res.status(200).json({message:`Delete User ${req.params.id}`})
+  const user = await User.findById(req.params.id)
+
+  if(!user) {
+    res.status(400)
+    throw new Error("User Not Found")
+  }
+
+  await user.deleteOne()
+
+  res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
---
+++ models/userModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: [true,'Please add a name'] },
+  email: { type: String, required: true, unique: true },
+  // password: { type: String, required: true },
+  // role: { type: String, enum: ['student', 'trainer', 'admin'], required: true },
+},
+{
+  timestamps: true,
+}
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
